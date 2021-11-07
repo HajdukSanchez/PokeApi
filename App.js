94f7eff2c486172fd217dc6282a807c7873a4213
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-11-07 16:30:43 -0500

    :art: [ADD] Tab Navigation with icons

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import Navigation from './src/navigation/Navigation'
+import { Navigation } from './src/navigation/Navigation'
 
 export default function App() {
   return (
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,15 +1,45 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import { Image } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Account, Favorites, Pokedex } from '../screens'
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
-export default function Navigation() {
+export function Navigation() {
   return (
     <Navigator>
-      <Screen name='Account' component={Account} />
-      <Screen name='Favorites' component={Favorites} />
-      <Screen name='Pokedex' component={Pokedex} />
+      <Screen
+        name='Account'
+        component={Account}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => <Icon name='user' color={color} size={size} />,
+        }}
+      />
+      <Screen
+        name='Pokedex'
+        component={Pokedex}
+        options={{ tabBarLabel: '', tabBarIcon: () => renderPokeBall() }}
+      />
+      <Screen
+        name='Favorites'
+        component={Favorites}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => <Icon name='heart' color={color} size={size} />,
+        }}
+      />
     </Navigator>
   )
+}
+
+function renderPokeBall() {
+  const styles = {
+    width: 50,
+    height: 50,
+    top: -18,
+  }
+
+  return <Image source={require('../assets/icon.png')} style={styles} />
 }
