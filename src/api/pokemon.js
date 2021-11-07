c595a01928f9d700beb55a62420ea590a48951ad
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-11-07 17:48:01 -0500

    :art: [ADD] Api changes

---
+++ src/api/pokemon.js
@@ -0,0 +1,12 @@
+import { API_HOST } from '../utils/constants'
+
+export async function getPokemon() {
+  try {
+    const url = `${API_HOST}/pokemon?limit=20&offset=0` // ? Limit of 20 pokemon per call
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(error)
+  }
+}
--- src/screens/pokedexScreen/PokedexScreen.js
+++ src/screens/pokedexScreen/PokedexScreen.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { getPokemon } from '../../api/pokemon'
 
 export function PokedexScreen() {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    ;(async () => await loadPokemons())()
+  }, [])
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemon()
+      setPokemons(pokemons)
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
