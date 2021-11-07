0252a486104eec5b6e890e4b40552c2c021783f3
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-11-07 18:06:37 -0500

    :art: [ADD] Pokemon detail

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -10,3 +10,13 @@ export async function getPokemon() {
     throw new Error(error)
   }
 }
+
+export async function getPokemonDetailByUrl(url) {
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(error)
+  }
+}
--- src/screens/pokedexScreen/PokedexScreen.js
+++ src/screens/pokedexScreen/PokedexScreen.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { getPokemon } from '../../api/pokemon'
+// * API
+import { getPokemon, getPokemonDetailByUrl } from '../../api/pokemon'
 
 export function PokedexScreen() {
   const [pokemons, setPokemons] = useState([])
@@ -13,7 +14,20 @@ export function PokedexScreen() {
   const loadPokemons = async () => {
     try {
       const response = await getPokemon()
-      setPokemons(pokemons)
+      response.results.forEach(async (pokemon) => {
+        const pokemonDetail = await getPokemonDetailByUrl(pokemon.url) // * Get pokemon detail
+        setPokemons((pokemons) => [
+          ...pokemons, // * Add pokemon to array
+          {
+            id: pokemonDetail.id,
+            name: pokemonDetail.name,
+            type: pokemonDetail.types[0],
+            order: pokemonDetail.order,
+            image: pokemonDetail.sprites.other['official-artwork'].front_default,
+          },
+        ])
+      })
+      console.log(pokemons[0])
     } catch (error) {
       throw new Error(error)
     }
