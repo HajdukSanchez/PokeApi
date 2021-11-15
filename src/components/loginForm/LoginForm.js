3e3e9c7f05e893764039e6e01cc4121e46809338
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-11-15 12:24:48 -0500

    :art: [ADD] Login FORM

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
 
 export function LoginForm() {
   return (
     <View>
-      <Text>Login Form</Text>
+      <Text style={styles.title}>Log in</Text>
+      <TextInput style={styles.input} placeholder='User Name' autoCapitalize='none' />
+      <TextInput
+        style={styles.input}
+        placeholder='Password'
+        autoCapitalize='none'
+        secureTextEntry={true}
+      />
+      <Button style={styles.button} title='Log in' onPress={() => console.log('Enter')} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 28,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+  button: {
+    margin: 12,
+    borderRadius: 10,
+  },
+})
