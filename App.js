26e59869615a7ff4f276edacb221279c2c323d88
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-11-17 06:58:05 -0500

    :art: [ADD] Pre-building configuration

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { TabNavigation } from './src/navigation'
+import { TabNavigation } from './src/navigation/tabNavigation/TabNavigation'
 import { AuthProvider } from './src/context/AuthContext'
 
 export default function App() {
